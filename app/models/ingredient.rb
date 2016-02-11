f9f213282a47bf9f664811497c1816336edd7524
victor fulconis <victor.fulconis@gmail.com>
2016-02-11 11:52:09 +0100

    finishing seed

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,2 +1,15 @@
 class Ingredient < ActiveRecord::Base
+  has_many :doses
+  before_destroy :check_dose
+  has_many :cocktails, through: :doses
+  validates :name, presence: true, uniqueness: true
+
+  def check_dose
+    if doses.count > 0
+      errors.add_to_base("cannot delete ingredient while doses exist")
+      return false
+    end
+  end
 end
+
+# dependent: :restrict_with_exception
